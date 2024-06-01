4ba82b0126809908310250eb92178ed661eb942e
Alfiya Sama <121941528+alfiyasama@users.noreply.github.com>
2024-06-01 15:53:04 +0530

    Create datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,107 @@
+// Java has 8 primitive data types; char, boolean, byte, short, int, long, float, and double. For this exercise, we'll work with the primitives used to hold integer values (byte, short, int, and long):
+
+// A byte is an 8-bit signed integer.
+// A short is a 16-bit signed integer.
+// An int is a 32-bit signed integer.
+// A long is a 64-bit signed integer.
+// Given an input integer, you must determine which primitive data types are capable of properly storing that input.
+
+// To get you started, a portion of the solution is provided for you in the editor.
+
+// Reference: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+
+// Input Format
+
+// The first line contains an integer, , denoting the number of test cases.
+// Each test case, , is comprised of a single line with an integer, , which can be arbitrarily large or small.
+
+// Output Format
+
+// For each input variable  and appropriate primitive , you must determine if the given primitives are capable of storing it. If yes, then print:
+
+// n can be fitted in:
+// * dataType
+// If there is more than one appropriate data type, print each one on its own line and order them by size (i.e.: ).
+
+// If the number cannot be stored in one of the four aforementioned primitives, print the line:
+
+// n can't be fitted anywhere.
+// Sample Input
+
+// 5
+// -150
+// 150000
+// 1500000000
+// 213333333333333333333333333333333333
+// -100000000000000
+// Sample Output
+
+// -150 can be fitted in:
+// * short
+// * int
+// * long
+// 150000 can be fitted in:
+// * int
+// * long
+// 1500000000 can be fitted in:
+// * int
+// * long
+// 213333333333333333333333333333333333 can't be fitted anywhere.
+// -100000000000000 can be fitted in:
+// * long
+
+
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+class Solution{
+    public static void main(String []argh)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();  // Number of test cases
+
+        for (int i = 0; i < t; i++) {
+            try {
+                BigInteger n = scanner.nextBigInteger();
+
+                boolean canBeFitted = false;
+                StringBuilder result = new StringBuilder();
+
+                // Check each data type
+                if (n.compareTo(BigInteger.valueOf(Byte.MIN_VALUE)) >= 0 && n.compareTo(BigInteger.valueOf(Byte.MAX_VALUE)) <= 0) {
+                    result.append("* byte\n");
+                    canBeFitted = true;
+                }
+                if (n.compareTo(BigInteger.valueOf(Short.MIN_VALUE)) >= 0 && n.compareTo(BigInteger.valueOf(Short.MAX_VALUE)) <= 0) {
+                    result.append("* short\n");
+                    canBeFitted = true;
+                }
+                if (n.compareTo(BigInteger.valueOf(Integer.MIN_VALUE)) >= 0 && n.compareTo(BigInteger.valueOf(Integer.MAX_VALUE)) <= 0) {
+                    result.append("* int\n");
+                    canBeFitted = true;
+                }
+                if (n.compareTo(BigInteger.valueOf(Long.MIN_VALUE)) >= 0 && n.compareTo(BigInteger.valueOf(Long.MAX_VALUE)) <= 0) {
+                    result.append("* long\n");
+                    canBeFitted = true;
+                }
+
+                if (canBeFitted) {
+                    System.out.println(n + " can be fitted in:");
+                    System.out.print(result.toString());
+                } else {
+                    System.out.println(n + " can't be fitted anywhere.");
+                }
+
+            } catch (Exception e) {
+                // Handling any unexpected exceptions.
+                System.out.println(scanner.next() + " can't be fitted anywhere.");
+            }
+        }
+
+        scanner.close();
+}
+}
+
+
+
